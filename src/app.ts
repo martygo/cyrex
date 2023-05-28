2ef5830ac9dda7ae11d1382a72e802dd78e2695e
Martins Gouveia <gouveiamartins6@gmail.com>
2023-05-28 13:11:39 -0300

    🎉 first commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+const DIRECTORY = "dist";
+
+let filesNames: string[] = [];
+
+if (fs.existsSync(DIRECTORY)) {
+	if (fs.readdirSync(DIRECTORY).length != 0) {
+		try {
+			const files = fs.readdirSync(DIRECTORY);
+
+			files.forEach((file) => {
+				if (file.includes(".html")) {
+					file = file.split(".")[0];
+
+					filesNames.push(file);
+				}
+			});
+		} catch (error) {
+			console.log("Error reading files", error);
+		}
+	}
+} else {
+	console.log("Folder 'dist' doesn't exist");
+	process.exit(1);
+}
+
+console.log(filesNames);
